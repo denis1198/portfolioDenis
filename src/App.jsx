a2e4c685c8f58f075286267f85da29f1dd5ee2ea
Denis <reisdenis98@gmail.com>
2024-02-22 23:00:19 -0300

    criação nova parte

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Dropdown from "./components/Dropdown/Dropdown";
 import About from "./components/About/About";
+import Portfolio from "./components/portfolio/Portfolio";
 function App() {
   return (
     <>
       <Dropdown />
       <Header />
       <About />
+      <Portfolio />
     </>
   );
 }
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import "./Portfolio.css";
+export default function Portfolio() {
+  return (
+    <div className="portfolioSection">
+      <div className="firstSectionPortfolio">
+        <h1>Meus projetos e projetos que trabalho/trabalhei.</h1>
+        <div className="containerProjects"></div>
+      </div>
+    </div>
+  );
+}
